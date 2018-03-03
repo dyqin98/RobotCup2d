b7950c39fb6b277146e514e947d4c3b52fa6654b
abstract <1550126414@qq.com>
2018-03-03 10:20:03 +0800

    初始化RobotCup2d仓库

---
+++ start.sh
@@ -0,0 +1,44 @@
+sudo apt-get update
+y
+sudo apt-get install nautilus-open-terminal 
+y
+sudo apt-get install build-essential 
+y
+sudo apt-get install xorg-dev  
+y
+sudo apt-get install flex bison 
+y
+sudo apt-get install libboost-dev libboost-all-dev
+y
+sudo apt-get install libqt4-dev libqt4-gui qt4-dev-tools qt4-doc qt4-designer qt4-qtconfig 
+y
+sudo apt-get install libpng12-dev
+y
+sudo apt-get install libglib2.0-dev 
+y
+sudo apt-get install libaudio-dev
+y
+sudo apt-get install libxt-dev libsm-dev
+y
+sudo apt-get install libxi-dev
+y
+sudo apt-get install libxrender-dev
+y
+sudo apt-get install libfreetype6-dev
+y
+sudo apt-get install libfontconfig1-dev
+y
+sudo apt-get install tcsh
+y
+sudo apt-get install libqt4-dev
+y
+sudo apt-get install libqt4-dev-bin
+y
+sudo apt-get install libqt4-opengl-dev
+y
+sudo apt-get install libqtwebkit-dev
+y
+sudo apt-get install qt4-linguist-tools
+y
+sudo apt-get install qt4-qmake
+y
